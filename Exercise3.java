93cec2a13f799f6de22271d8cd0a555a3f387397
Timothy Luckcock <39391015+thecodehero@users.noreply.github.com>
2021-02-24 19:53:34 +0000

    Add files via upload

---
+++ Exercise3.java
@@ -0,0 +1,57 @@
+	
+/* ***************************************
+   AUTHOR: Timothy Luckcock
+		   ***WRITE A SHORT DESCRIPTION OF WHAT YOUR MODIFIED PROGRAM DOES HERE.
+	   
+	   This is a program that tells people the price of different kinds of snacks for a vending machine. 
+	   The program display choices to the user with letters representing each and the users enters the 
+	   letter next to the drink that they want. The program should know the snacks and prices of each
+	   letter. If an incorrect choice is typed a message informing the user that their is nothing for that 
+	   choice available should be printed. 
+   ****************************************/
+
+class vendingmachine
+    public static void main (String[] param)
+    {
+
+		userChoice();  // ***REPLACE THE WORD DOTHIS WITH THE NAME YOU USE BELOW
+		NOWDOTHIS();  // ***REPLACE THE WORD NOWDOTHIS WITH THE 2ND NAME YOU USE BELOW
+		
+		System.exit(0);
+		
+    } // END main
+
+	
+	/* ***************************************
+	*
+	*	***PUT A COMMENT HERE TO EXPLAIN WHAT THIS METHOD IS FOR
+	*/
+	
+    public static void userChoice ()  // ***REPLACE THE NAME 'DOTHIS' WITH THE NAME YOU USED ABOVE
+	                              // ***THE NAME SHOULD HELP THE READER UNDERSTAND WHAT THIS CODE DOES
+    {
+ 
+        // *** DELETE THIS LINE. THE ACTUAL CODE GOES IN HERE - THE INSTRUCTIONS THAT DO THINGS
+		
+    } // END DOTHIS            
+	         // ****DONT FORGET TO CHANGE 'DOTHIS' HERE TO YOUR METHOD NAME
+	
+		
+	/* ***************************************
+	*
+	*	***PUT A COMMENT HERE TO EXPLAIN WHAT THIS METHOD IS FOR
+	*/
+	
+    public static void NOWDOTHIS ()  // ***REPLACE THE NAME 'NOWDOTHIS' WITH THE NAME YOU USED ABOVE
+	                              // ***THE NAME SHOULD HELP THE READER UNDERSTAND WHAT THIS CODE DOES
+    {
+ 
+        // *** DELETE THIS LINE. THE ACTUAL CODE GOES IN HERE - THE INSTRUCTIONS THAT DO THINGS
+		
+    } // END NOWDOTHIS           
+	         // ****DONT FORGET TO CHANGE 'NOWDOTHIS' HERE TO YOUR METHOD NAME
+	
+
+	
+} // END class vendingmachine
+               // ***DONT FORGET TO CHANGE 'template' HERE TO YOUR CLASS NAME FROM THE TOP
