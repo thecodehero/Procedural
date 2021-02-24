f0b6d9c4c767f856308216fd044f3b484d5b4ccb
Timothy Luckcock <39391015+thecodehero@users.noreply.github.com>
2021-02-24 19:53:49 +0000

    Add files via upload

---
+++ template (1).java
@@ -0,0 +1,38 @@
+	
+/* ***************************************
+   AUTHOR: Timothy Luckcock
+		   ***WRITE A SHORT DESCRIPTION OF WHAT YOUR MODIFIED PROGRAM DOES HERE.  
+	  
+   ****************************************/
+
+class template // 
+{
+    public static void main (String[] param)
+    {
+
+		DOTHIS();  // 
+		NOWDOTHIS();  // 
+		
+		System.exit(0);
+		
+    } // END main
+
+	
+	/* ***************************************
+	*
+	*	***PUT A COMMENT HERE TO EXPLAIN WHAT THIS METHOD IS FOR
+	*/
+	
+    public static void DOTHIS ()   
+	                              
+    {
+ 
+        
+		
+    } // END DOTHIS          
+    public static void NOWDOTHIS ()  	                             
+    {       
+		
+    } // END NOWDOTHIS      
+} // END class template  
+             
