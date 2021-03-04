fc33c94281dce6af6565669b2d4b717894637247
Timothy Luckcock <39391015+thecodehero@users.noreply.github.com>
2021-03-04 00:32:16 +0000

    Add files via upload

---
+++ recursivecalculator.java
@@ -0,0 +1,18 @@
+/* ***************************************
+   AUTHOR: Timothy Luckcock
+		   The purpose of this program is to recursively pase expressions, input as strings, from the follining defined languages 
+		   and calculates and prints out the answer to the calculations.			
+	      ****************************************/
+import java.util.Scanner //import scanner library
+class recursivecalculator // ***THE WORD AFTER CLASS MUST BE THE NAME OF THE FILE (BEFORE the .java) YOU SAVE THIS IN
+{
+    public static void main (String[] param)
+    {		
+		
+		System.exit(0);
+		
+    } // END main
+
+	
+} // END class recursive calculator
+               
